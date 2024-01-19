68a7bb95a85589b99f4cc10f7f64e99b3d552a08
rajatsaini18 <137900399+rajatsaini18@users.noreply.github.com>
2024-01-19 23:35:09 +0530

    Updates

---
+++ precache-manifest.1e1b82b227aad9999dbf281fe12730dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf37c217d1da46670c535bcfb5d9385",
+    "url": "/react-application/index.html"
+  },
+  {
+    "revision": "e49b93b781c0a52343ad",
+    "url": "/react-application/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "702d9a6e081d1421623e",
+    "url": "/react-application/static/js/2.8b3be0e5.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/react-application/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "e49b93b781c0a52343ad",
+    "url": "/react-application/static/js/main.628ef3f9.chunk.js"
+  },
+  {
+    "revision": "d7769e0e3026505ae13c",
+    "url": "/react-application/static/js/runtime~main.961102fb.js"
+  }
+]);
